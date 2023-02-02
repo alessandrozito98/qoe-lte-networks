1f05073ab0798affc8b952ca5f3468eed71d1be0
Alessandro Zito <alessandro4.zito@mail.polimi.it>
2023-02-02 17:01:08 +0100

    refactor: ready to publish the repo

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # QoE prediction on LTE Networks
+
+[![License: GPL3.0](https://img.shields.io/badge/License-GPL-brightgreen.svg)](https://choosealicense.com/licenses/gpl-3.0/)
+<!--[![CodeFactor](https://www.codefactor.io/repository/github/alessandrozito98/simulatoresistemielettorali-2/badge)](https://www.codefactor.io/repository/github/alessandrozito98/simulatoresistemielettorali-2)-->
+<!--- 
+[![wakatime](https://wakatime.com/badge/github/alessandrozito98/SimulatoreSistemiElettorali-2.svg)](https://wakatime.com/badge/github/alessandrozito98/SimulatoreSistemiElettorali-2) 
+-->
+
+
+## Description
+
+A Supervised Machine Learning Classifier method to predict users’ satisfaction related to the usage of YouTube Service.
+
+
+## Installation and Configuration
+
+Needs python3.
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install all the librabries.
+
+```bash
+pip3 install -r requirements.txt
+```
+
+
+### Usage
+
+```bash
+python3 -m src LeggiElettorali/LawName
+``` 
+
+
+## Credits
+
+All the documents used ar in the [docs](https://github.com/alessandrozito98/qoe-lte-networks/tree/master/docs)
+
+
+## Authors
+
+Alessandro Zito ([@alessandrozito98](https://github.com/alessandrozito98))
+
+Zhengchen Xu ([@LonghairXu](https://github.com/LonghairXu))
+
+
+### Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## License
+Licensed under [GNU General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/) license.
--- main.py
+++ main.py
@@ -1,68 +1,91 @@
-from pkgutil import extend_path
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
+import numpy as np
 import pandas as pd
 
-# scaler = StandardScaler()
-
-path ='data/'
+path = 'data/'
 
 basic_train = pd.read_csv(path + 'BasicDataset_Training_MRN.csv')
 basic_test = pd.read_csv(path + 'BasicDataset_Test_MRN.csv')
 
+
+new_feature_train = pd.DataFrame(data=(basic_train.Cumulative_YoutubeSess_LTE_DL_Volume + basic_train.Cumulative_YoutubeSess_UMTS_DL_Volume) / (basic_train.Cumulative_YoutubeSess_LTE_DL_Time + basic_train.Cumulative_YoutubeSess_UMTS_DL_Time).round(1),columns=['new_feature'])
+new_feature_train['new_feature'].fillna(0,inplace=True)
+new_feature_test = pd.DataFrame(data=(basic_test.Cumulative_YoutubeSess_LTE_DL_Volume + basic_test.Cumulative_YoutubeSess_UMTS_DL_Volume) / (basic_test.Cumulative_YoutubeSess_LTE_DL_Time + basic_test.Cumulative_YoutubeSess_UMTS_DL_Time).round(1),columns=['new_feature'])
+new_feature_test['new_feature'].fillna(0,inplace=True)
+
+# create new feature CDF
+# cdf_train = basic_train.Cumulative_Lim_Service_Time_LTE
+# cdf_test = basic_train.loc[:, 'User_Satisfaction'].copy()
+# yt = cdf_test.copy()
+# bin_edges = np.linspace(min(cdf_train), max(cmf_train), 101)
+# neg, _ = np.histogram(cdf_train[yt == +1], bins=bin_edges)  # count number of evidences per bin
+# pos, _ = np.histogram(cdf_train[yt == 0], bins=bin_edges)
+# sumpos =  sum(pos)
+# sumneg =  sum(neg)
+# pos = pos.astype(float) / sumpos  # normalize to total number of evidences
+# neg = neg.astype(float) / sumneg
+# xrange = bin_edges[1:] - bin_edges[:1]
+# plt.plot(xrange,np.cumsum(pos))
+# plt.plot(xrange,np.cumsum(neg))
+# plt.show()
+#
 train = basic_train.drop(['User_Satisfaction','Unnamed: 0','Unnamed: 0.1'],axis = 1)
+train = train.join(new_feature_train,how='right')
 test = basic_test.drop(['User_Satisfaction','Unnamed: 0','Unnamed: 0.1'],axis = 1)
+test = test.join(new_feature_test,how='right')
 ground_truth_train = basic_train.loc[:,'User_Satisfaction'].copy()
 ground_truth_test = basic_test.loc[:,'User_Satisfaction'].copy()
+
 
 # Yeo Johnson Transformation, make it closed to Gaussian distribution
 from sklearn.preprocessing import PowerTransformer
 pt = PowerTransformer(method='yeo-johnson', standardize=True,)
-
+#
 train = pt.fit_transform(train)
 test = pt.fit_transform(test)
 
-# seems like no need to split the validation dataset when I did the test
-# train = scaler.fit_transform(train)
-# x_train, x_val, y_train, y_val = train_test_split(train,ground_truth_train, test_size=0.2)
+# Plot Distribution of Cum. Full Service Time in LTE (no used)
+# fig, ax = plt.subplots(1, 1, figsize=(20, 5))
+# x = basic_train.Cumulative_Full_Service_Time_UMTS
+# tmp_0 = ax.hist(x, bins=100)
+# ax.set_ylabel('Bincount')
+# ax.xaxis.label.set_color('red')
+# ax.yaxis.label.set_color('red')
+# ax.tick_params(axis='x', colors='red')
+# ax.tick_params(axis='y', colors='red')
+# plt.show()
 
-# establish Keras model
+x_train, x_val, y_train, y_val = train_test_split(train,ground_truth_train, test_size=0.2)
+# # establish Keras model
 model = Sequential()
 
-model.add(Dense(16, activation='relu', input_shape=(len(train),12)))
-model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu', input_shape=(len(train),13)))
+model.add(Dense(32,activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
-#
-# callbacks_list = [
-#     keras.callbacks.ModelCheckpoint(
-#         filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
-#         monitor='val_loss', save_best_only=True),
-#     keras.callbacks.EarlyStopping(monitor='acc', patience=1)]
-# model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy, metrics=["accuracy"])
-
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(train,ground_truth_train,batch_size=10,validation_data=(test,ground_truth_test),epochs=30,verbose=1)
+model.fit(x_train,y_train,batch_size=10,validation_data=(x_val,y_val),epochs=30,verbose=1)
 
-# from keras.models import load_model
-# model=load_model('best_model.17-0.60.h5')
-
-# evaluate the model
-score=model.evaluate(test,ground_truth_test,verbose=2)
-print('Test loss:',score[0])
-print('Test accuracy:',score[1])
-
-#prepare for the AUC-ROC curve
+# print(model.summary())(no used)
+# # from keras.models import load_model
+# # model=load_model('best_model.17-0.60.h5')
+#
+# # evaluate the model
+# score=model.evaluate(test,ground_truth_test,verbose=2)
+# print('Test loss:',score[0])
+# print('Test accuracy:',score[1])
+#
+#prepare for the AUC-ROC curve(used)
 from sklearn.metrics import roc_curve
 y_pred_keras = model.predict(test).ravel()
 fpr_keras, tpr_keras, thresholds_keras = roc_curve(ground_truth_test, y_pred_keras)
 
-from sklearn.ensemble import RandomForestClassifier
 # Supervised transformation based on random forests
+from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier(max_depth=10, n_estimators=40)
 rf.fit(train, ground_truth_train)
 
@@ -81,5 +104,4 @@ plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 plt.title('ROC curve')
 plt.legend(loc='best')
-plt.savefig('./roc_curve.png')
 plt.show()
